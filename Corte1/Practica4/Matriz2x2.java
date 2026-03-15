fdf6e040516dc60af22838a044da35ca40ea1b33
sergioacastellanosc <sergiocastellanos6767@gmail.com>
2026-03-15 11:33:33 -0500

    Add files via upload

---
+++ Corte1/Practica4/Matriz2x2.java
@@ -0,0 +1,55 @@
+package matriz2x2;
+import java.util.Scanner;
+
+
+public class Matriz2x2 
+{
+    public int[][] data;
+    public int filas;
+    public int columnas;
+
+    public Matriz2x2() 
+    {
+        filas = 2;
+        columnas = 2;
+        data = new int[filas][columnas];
+    }
+
+    public void getvector(String name) 
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        
+        for (int i = 0; i < filas ; i++) 
+        {
+            for (int j = 0 ; j < columnas ; j++)
+            {
+                System.out.print("data[" + i + "][" + j + "]:");
+                data[i][j] = sc.nextInt();
+                
+            }
+        }
+        
+    }
+
+    public void setvector()
+    {
+        
+        for ( int i = 0; i < filas ; i++)
+        {
+            for ( int j = 0 ; j < columnas ; j++)
+            {
+                System.out.print(data[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+    public static void main(String[] args) 
+    {
+        Matriz2x2 m = new Matriz2x2();
+        m.getvector("My_matriz");
+        m.setvector();
+        
+    }
+}
